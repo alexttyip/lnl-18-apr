5862c092fc7d7d74513848bd4b5c059146d6fd8f
Alex Yip <tsztoyip@gmail.com>
2024-04-18 00:58:48 +0100

    Use is

--- index.ts
+++ index.ts
@@ -5,14 +5,25 @@ type UserResponse = {
   favouriteNumbers: number[];
 };
 
+const validateUserResponse = (res: unknown): res is UserResponse => {
+  return (
+    typeof (res as UserResponse).name === "string" &&
+    Array.isArray((res as UserResponse).favouriteNumbers)
+  );
+};
+
 const response = await fetch("https://jsonplaceholder.typicode.com/users/1");
 
-// We're saying to TS "Trust me, `json` is of shape `UserResponse`"
-const json = (await response.json()) as UserResponse;
+// `json` is implicitly `any`
+const json = await response.json();
 
-// TS will now provide type-checking and intellisense based on the specified type
-const name = json.name;
-const favouriteNumbers = json.favouriteNumbers;
+if (validateUserResponse(json)) {
+  // All good! The type of json is narrowed to `UserResponse`
+  // TS will now provide type-checking and intellisense based on the type
+  const { name, favouriteNumbers } = json;
 
-console.log(name);
-console.log(favouriteNumbers[0]);
+  console.log(name);
+  console.log(favouriteNumbers[0]);
+} else {
+  console.log("uh oh");
+}
